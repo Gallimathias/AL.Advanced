bf0058a7b4008a57d677b113d9289ed249167242
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2016-10-26 17:30:07 +0200

    NAV_API: Add Database API
    - Add Nav_Database
     -- Add Database for Database Connection
     -- Add Metadata
     -- Add Object
    
    :: first simple implementation

---
+++ Nav_API/NAV_Database/Metadata.cs
@@ -0,0 +1,19 @@
+﻿using Nav_API.Interfaces;
+using System.Data.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Nav_API.NAV_Database
+{
+    internal class Metadata
+    {
+        private IObjectMetadata metadata;
+        
+
+        public Metadata(IObjectMetadata object_metadata)
+        {
+            metadata = object_metadata;
+        }
+        
+    }
+}
