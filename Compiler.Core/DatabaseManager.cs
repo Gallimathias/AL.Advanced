b459912e3c6740d4c5350fb4a8bb269484578263
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2016-10-27 07:27:23 +0200

    Compiler.Core: Add DatabaseManager

---
+++ Compiler.Core/DatabaseManager.cs
@@ -0,0 +1,24 @@
+﻿using Nav_API.NAV_Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Core
+{
+    public static class DatabaseManager
+    {
+        static Dictionary<string, Database> databases;
+
+        public static void Initzalitation()
+        {
+            databases = new Dictionary<string, Database>();
+        }
+
+        public static void AddDatabase(Database database)
+        {
+            databases.Add(database.ToString(), database);
+        }
+    }
+}
