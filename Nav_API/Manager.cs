d1ba6479882b3ecb6af0c080783104fea71f8d6d
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2016-10-27 11:53:31 +0200

    Nav_API: Add Manager
    - Add Manger for managing the API

---
+++ Nav_API/Manager.cs
@@ -0,0 +1,17 @@
+﻿using Nav_API.NAV_Database;
+using Nav_API.SQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nav_API
+{
+    public static class Manager
+    {
+        public static Database GetDatabase(string connection) 
+            => new Database(new Nav_DatabaseDataContext(connection));
+
+    }
+}
