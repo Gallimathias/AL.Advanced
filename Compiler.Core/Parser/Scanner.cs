dce5a4bc8ec299a5a298b4b0978382fc0a5a9047
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-07-17 06:20:07 +0200

    Add Scanner

---
+++ Compiler.Core/Parser/Scanner.cs
@@ -0,0 +1,43 @@
+﻿using Compiler.Core.Syntax;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Core.Parser
+{
+    public class Scanner
+    {
+        private MemberDeclarationSyntax[] tokens;
+        private Dictionary<int, SyntaxParsDelegate> syntaxDictionary;
+
+        public Scanner(params MemberDeclarationSyntax[] tokens)
+        {
+            this.tokens = tokens;
+        }
+
+        public void Scan()
+        {
+            foreach (var token in tokens)
+            {
+                Analyse(token);
+            }
+        }
+
+        private MemberSyntax Analyse(MemberDeclarationSyntax memberDeclaration)
+        {
+            foreach (var syntax in syntaxDictionary)
+            {
+                if (syntax.Value(memberDeclaration, Analyse, out MemberSyntax memberSyntax))
+                    return memberSyntax;
+
+                continue;
+            }
+
+            throw new Exception("No valid expression");
+        }
+    }
+}
---
+++ Compiler.Core/Tokenizer/TokenScanner.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Core.Tokenizer
+{
+    public class TokenScanner
+    {
+        private CompilationUnitSyntax root;
+        public TokenScanner(string source)
+        {
+            root = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
+        }
+
+        public MemberDeclarationSyntax[] Scan()
+        {
+            var space = (NamespaceDeclarationSyntax)root.Members.FirstOrDefault(m => m.Kind() == SyntaxKind.NamespaceDeclaration);
+            return space.Members.ToArray();
+        }
+    }
+}
