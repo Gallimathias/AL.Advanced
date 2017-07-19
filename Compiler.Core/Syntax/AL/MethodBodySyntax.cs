9f2f4f6fdff139c07eb6d311e2f47b239fd9a8ce
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-07-19 06:31:13 +0200

    Add MethodBodySyntax

---
+++ Compiler.Core/Syntax/AL/MethodBodySyntax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+
+namespace Compiler.Core.Syntax.AL
+{
+    public class MethodBodySyntax : MemberSyntax
+    {
+        ClassDeclarationSyntax classDeclaration;
+
+        public MethodBodySyntax()
+        {
+
+        }
+        private MethodBodySyntax(MethodBodySyntax methodBodySyntax)
+        {
+            classDeclaration = methodBodySyntax.classDeclaration;
+        }
+
+        public override bool TryParse(MemberDeclarationSyntax memberDeclaration,
+            Func<MemberDeclarationSyntax, MemberSyntax> analyser, out MemberSyntax memberSyntax)
+        {
+            memberSyntax = null;
+
+            if(memberDeclaration is ClassDeclarationSyntax classDeclaration)
+            {
+                if (!ContainsBaseType(classDeclaration.BaseList.Types))
+                    return false;
+
+                this.classDeclaration = classDeclaration;
+                memberSyntax = new MethodBodySyntax(this);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ContainsBaseType(SeparatedSyntaxList<BaseTypeSyntax> types)
+        {
+            foreach (var type in types)
+            {
+                if (((IdentifierNameSyntax)type.Type).Identifier.Text != "NavMethodScope")
+                    continue;
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
