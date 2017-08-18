1249598b1ad7c3b568b56431b95d22e9af973638
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-08-18 08:02:22 +0200

    Add Method Syntax
    Remove unused Codeunit map
    Ad AlToAdcancedMemberMap

---
+++ Compiler.Core/Syntax/ALAdvanced/MethodSyntax.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Core.Syntax.ALAdvanced
+{
+    public class MethodSyntax : ALAdvancedSourceMemberSyntax<MethodDeclarationSyntax>
+    {
+        public string Identifier { get; set; }
+        public SyntaxKind Modifier { get; set; }
+        public bool IsOverride
+        {
+            get => @override == SyntaxKind.OverrideKeyword; set
+            {
+                if (value)
+                    @override = SyntaxKind.OverrideKeyword;
+                else
+                    @override = SyntaxKind.None;
+            }
+        }
+        public bool IsStatic
+        {
+            get => @static == SyntaxKind.StaticKeyword; set
+            {
+                if (value)
+                    @static = SyntaxKind.StaticKeyword;
+                else
+                    @static = SyntaxKind.None;
+
+            }
+        }
+
+        private SyntaxKind @override;
+        private SyntaxKind @static;
+
+        public override bool TryParse(MemberDeclarationSyntax memberDeclaration,
+            Func<MemberDeclarationSyntax, SyntaxMember> analyser, out SyntaxMember memberSyntax)
+        {
+            memberSyntax = null;
+
+            if (memberDeclaration is MethodDeclarationSyntax methodDeclaration)
+            {
+                var @override = methodDeclaration.Modifiers.FirstOrDefault(m => m.Kind() == SyntaxKind.OverrideKeyword).Kind();
+                var @static = methodDeclaration.Modifiers.FirstOrDefault(m => m.Kind() == SyntaxKind.StaticKeyword).Kind();
+
+                memberSyntax = new MethodSyntax
+                {
+                    CSharpMember = methodDeclaration,
+                    Identifier = (string)methodDeclaration.Identifier.Value,
+                    Modifier = methodDeclaration.Modifiers.First().Kind(),
+                    IsStatic = @static == SyntaxKind.StaticKeyword,
+                    IsOverride = @override == SyntaxKind.OverrideKeyword
+                };
+                return true;
+            }
+
+            return false;
+        }
+
+        internal override void Normalize()
+        {
+            if (Identifier.Length > 0)
+                Identifier = Identifier.Substring(0, 1).ToUpper() + Identifier.Substring(1);
+
+            CSharpMember = CSharpMember.NormalizeWhitespace();
+        }
+
+        internal override void ParseCSharp()
+        {
+            CSharpMember = SyntaxFactory.MethodDeclaration(SyntaxFactory.ParseTypeName("void"), Identifier)
+                .AddModifiers(SyntaxFactory.Token(Modifier))
+                .WithBody(SyntaxFactory.Block())
+                .NormalizeWhitespace();
+        }
+
+        public override string ToString() => $"Method {Identifier}";
+    }
+}
---
+++ Compiler.Core/Translators/Maps/AlToAdvancedMemberMap.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AL = Compiler.Core.Syntax.AL;
+using Advanced = Compiler.Core.Syntax.ALAdvanced;
+using System.Reflection;
+
+namespace Compiler.Core.Translators.Maps
+{
+    public class AlToAdvancedMemberMap
+    {
+
+        public MethodInfo[] Methods => GetType().GetMethods();
+        
+        
+        public object Invoke(string memberName, object member) => GetType().GetMethod(memberName).Invoke(this, new[] { member });
+
+        public object ObjectCtorSyntax(AL.ObjectCtorSyntax ctor) => null;
+
+        public Advanced.MethodSyntax MethodHeadSyntax(AL.MethodHeadSyntax head) {
+            var parent = (AL.CodeUnitSyntax)head.Parent;
+
+            var method = Copy<Advanced.MethodSyntax>(head);
+
+            var body = parent.Members.FirstOrDefault(m => {
+                if(m is AL.MethodBodySyntax bodySyntax)
+                {
+                    if (bodySyntax.Identifier == $"{head.Identifier}_Scope")
+                        return true;
+                }
+
+                return false;
+            });
+
+
+            return method;
+        }
+
+        public object MethodBodySyntax(AL.MethodBodySyntax body) => null;
+
+        public object PropertySyntax(AL.PropertySyntax property) => null;
+
+        private T Copy<T>(object source)
+        {
+            var target = Activator.CreateInstance<T>();
+
+            var sourceProps = source.GetType().GetProperties();
+            var targetProps = target.GetType().GetProperties();
+
+            foreach (var sourceProp in sourceProps)
+            {
+                if (!sourceProp.CanRead)
+                    continue;
+
+                var targetProp = targetProps.FirstOrDefault(p => p.Name == sourceProp.Name);
+
+                if (targetProp == null)
+                    continue;
+
+                if (!targetProp.CanWrite)
+                    continue;
+
+                targetProp.SetValue(target, sourceProp.GetValue(source));
+
+            }
+
+            return target;
+        }
+    }
+}
