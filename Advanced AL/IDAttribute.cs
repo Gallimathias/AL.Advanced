49b2bcb7128a8b9a585f3a515afdb4bb2da3f21c
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2017-08-18 07:59:12 +0200

    Add IDAttribute for Object Id's

---
+++ Advanced AL/IDAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class IDAttribute : Attribute
+    {
+        public int ID { get; private set; }
+
+        public IDAttribute(int id)
+        {
+            ID = id;
+        }
+    }
+}
