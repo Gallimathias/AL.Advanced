ee50fb4b49d0252036e031a41e473cacf778cd29
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2018-01-10 13:34:40 +0100

    Add view in Explorer with Prism
    * Add CodeUnits Page
    * Add Bootstrapper
    * Add MainWindow

--- Nav.Object.Explorer/App.xaml.cs
+++ Nav.Object.Explorer/App.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,48 @@ namespace Nav.Object.Explorer
     /// </summary>
     public partial class App : Application
     {
+        private Bootstrapper bootstrapper;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver(SearchType);
+
+            bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+
+            base.OnStartup(e);
+        }
+
+        private Type SearchType(Type viewType)
+        {
+            var baseName = "Nav.Object.Explorer";
+            var typeName = $"{viewType.Name}Base";
+            var extName = "";
+
+            var type = Type.GetType($"{baseName}.{typeName}, {viewType.Assembly.FullName}", false);
+
+            if (type != null)
+                return type;
+
+            extName = "Model";
+            type = Type.GetType($"{baseName}.{extName}.{typeName}, {baseName}.{extName}", false);
+
+            if (type != null)
+                return type;
+
+            extName = "Core";
+            type = Type.GetType($"{baseName}.{extName}.{typeName}, {baseName}.{extName}", false);
+
+            if (type != null)
+                return type;
+
+            var exception = new MissingMemberException($"{typeName} not found");
+
+            exception.Data.Add("baseName", baseName);
+            exception.Data.Add("typeName", typeName);
+            exception.Data.Add("extName", extName);
+
+            throw exception;
+        }
     }
 }
---
+++ Nav.Object.Explorer/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Microsoft.Practices.ServiceLocation;
+using Nav.Object.Explorer.Model;
+using Prism.DryIoc;
+
+
+namespace Nav.Object.Explorer
+{
+    internal class Bootstrapper : DryIocBootstrapper
+    {
+        protected Application CurrentApplication => Application.Current;
+        protected MainWindowBase ShellBase => (MainWindowBase)CurrentApplication.MainWindow.DataContext;
+
+        protected override DependencyObject CreateShell() 
+            => ServiceLocator.Current.GetInstance<MainWindow>();
+
+        protected override void InitializeShell()
+        {
+            ShellBase.CollectPages();
+            CurrentApplication.MainWindow = (MainWindow)Shell;           
+            CurrentApplication.MainWindow.Show();
+        }
+        
+    }
+}
