ad4579f9c099c89816b23d17cbe86e4c6baea7d4
Maximilian Krüger <Maximilian.Krueger@it.konicaminolta.de>
2018-01-10 13:34:59 +0100

    Add Core Functionality for Explorer

---
+++ Nav.Object.Explorer.Core/PageBase.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nav.Object.Explorer.Core
+{
+    public abstract class PageBase : BindableBase
+    {
+        public string Title { get => title; protected set => SetProperty(ref title, value); }
+
+        private string title;
+
+        public PageBase(string title)
+        {
+            Title = title.Replace("Base", "");
+        }
+    }
+}
