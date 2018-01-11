1c02ae887a3bd2b24fb7d72fdf1670cb7992b781
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-11 12:35:29 +0100

    Add CodeUnit definiton

---
+++ AL.Advanced.Definition.CSharpALFormatting/Object/CodeUnit.cs
@@ -0,0 +1,15 @@
+﻿using AL.Advanced.Core.Definition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AL.Advanced.Definition.CSharpALFormatting.Object
+{
+    internal class CodeUnit : ALObject
+    {
+        public CodeUnit() : base()
+        {
+            ObjectType = ObjectType.CodeUnit;
+        }
+    }
+}
