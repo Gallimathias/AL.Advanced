dd739ed8c8a71480a4e5b10e2e9a2a5b86ee71f5
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-11 14:21:54 +0100

    Insert Tree Syntax

---
+++ AL.Advanced.Definition.CSharpALFormatting/CSharpALTree.cs
@@ -0,0 +1,27 @@
+﻿using AL.Advanced.Core;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AL.Advanced.Definition.CSharpALFormatting
+{
+    public class CSharpALTree : Tree<CSharpALTree>
+    {
+        public static CSharpALTree Parse(string text)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool TryParse(string text, out CSharpALTree tree)
+        {
+            var list = CSharpSyntaxTree.ParseText(text).GetCompilationUnitRoot().Members;
+
+            foreach (NamespaceDeclarationSyntax namespaceDeclarationSyntax in list)
+            {
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
