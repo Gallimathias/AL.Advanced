7f42a0b64d77155aeb1a91dad6f31890adc6aba7
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-12 12:41:06 +0100

    Implement CodeUnit

--- AL.Advanced.Definition.CSharpALFormatting/Object/CodeUnit.cs
+++ AL.Advanced.Definition.CSharpALFormatting/Object/CodeUnit.cs
@@ -3,6 +3,10 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
 
 namespace AL.Advanced.Definition.CSharpALFormatting.Object
 {
@@ -15,22 +19,69 @@ namespace AL.Advanced.Definition.CSharpALFormatting.Object
 
         public override string ToText()
         {
-            throw new NotImplementedException();
+            var builder = new StringBuilder();
+            builder.AppendLine("[NavCodeunitOptions()]");
+            builder.AppendLine($"public sealed class Codeunit{ID} : NavCodeunit {{");
+            builder.AppendLine($"public Codeunit{ID}(ITreeObject parent) : base(parent, {ID}){{}}");
+            builder.AppendLine($"public override string ObjectName => @\"{Name}\";");
+            builder.AppendLine();
+            builder.AppendLine(
+                $"public static Codeunit{ID} __Construct(ITreeObject parent) => new Codeunit{ID}(parent); }}");
+            
+            return CSharpSyntaxTree
+                    .ParseText(builder.ToString())
+                    .GetCompilationUnitRoot()
+                    .NormalizeWhitespace()
+                    .ToFullString();
         }
 
         public override bool Check(MemberDeclarationSyntax root)
         {
-            throw new NotImplementedException();
+            if (root is ClassDeclarationSyntax classDeclaration)
+            {
+                var typeName = classDeclaration.BaseList.Types.FirstOrDefault()?.ToString();
+
+                if (string.IsNullOrWhiteSpace(typeName))
+                    return false;
+
+                return typeName.ToLower() == "navcodeunit";
+            }
+
+            return false;
         }
 
         public override void Parse(MemberDeclarationSyntax root)
         {
-            throw new NotImplementedException();
+            if (!TryParse(root))
+                throw new Exception("Could not Parse");
         }
 
         public override bool TryParse(MemberDeclarationSyntax root)
         {
-            throw new NotImplementedException();
+            if (!Check(root))
+                return false;
+
+            var classDeclaration = (ClassDeclarationSyntax)root;
+            ID = int.Parse(((string)classDeclaration.Identifier.Value).Substring(8));
+
+            var objNameField = classDeclaration
+                                .Members
+                                .Select(m => m is PropertyDeclarationSyntax property ? property : null)
+                                .Where(m => m != null)
+                                .First(m => "ObjectName" == (string)m.Identifier.Value);
+
+            Name = objNameField
+                     .AccessorList
+                     .Accessors
+                     .First(a => a.Kind() == SyntaxKind.GetAccessorDeclaration)
+                     .ToFullString();
+
+            var start = Name.IndexOf('"') + 1;
+
+            Name = Name.Substring(start, Name.IndexOf('"', start) - start);
+
+            return true;
         }
+        
     }
 }
