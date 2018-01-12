3e61c708d4d982165eb6c3c5387f78b3bc737257
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-12 12:40:04 +0100

    Add UnitRoot

---
+++ AL.Advanced.Core/UnitRoot.cs
@@ -0,0 +1,32 @@
+﻿using AL.Advanced.Core.Definition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AL.Advanced.Core
+{
+    public class UnitRoot
+    {
+        public List<Member> Members { get; private set; }
+
+        public UnitRoot(params Member[] members)
+        {
+            Members = new List<Member>(members);
+        }
+        public UnitRoot(List<Member> list) : this(list.ToArray())
+        {
+        }
+
+        public string ToText()
+        {
+            var strb = new StringBuilder();
+
+            foreach (var member in Members)
+            {
+                strb.AppendLine(member.ToText());
+            }
+
+            return strb.ToString();
+        }
+    }
+}
