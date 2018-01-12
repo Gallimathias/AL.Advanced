9dc7d9c808dfc64967b5a267f75e0fc92c04aa91
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-12 14:57:02 +0100

    Implement default tree

--- AL.Advanced.Core/Tree.cs
+++ AL.Advanced.Core/Tree.cs
@@ -6,5 +6,13 @@ namespace AL.Advanced.Core
 {
     public abstract class Tree<TChildType>
     {
+        public UnitRoot UnitRoot { get; protected set; }
+        
+        protected Tree(UnitRoot unitRoot)
+        {
+            UnitRoot = unitRoot;
+        }
+
+        public virtual string ToText() => UnitRoot.ToText().Trim();
     }
 }
