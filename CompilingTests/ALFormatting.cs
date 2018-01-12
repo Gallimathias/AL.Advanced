f98875c46e3b900b07dfc1ec14345f8f36f9da59
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-12 09:58:18 +0100

    Add Compiling Test

---
+++ CompilingTests/ALFormatting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using AL.Advanced.Definition.CSharpALFormatting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CompilingTests
+{
+    [TestClass]
+    public class ALFormatting
+    {
+        [TestMethod]
+        public void CompileExample()
+        {
+            var text = File.ReadAllText(@"..\..\..\examples\CSharp-ALFormatting\CodeUnit.cs");
+
+            CSharpALTree.Parse(text);
+        }
+    }
+}
