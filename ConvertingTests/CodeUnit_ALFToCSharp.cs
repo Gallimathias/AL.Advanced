21cca53dacbe5e75d16da30876ad91e86eccd980
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-01-14 01:11:23 +0100

    Add new tests

---
+++ ConvertingTests/CodeUnit_ALFToCSharp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Al.Advanced.Definition.CSharp;
+using AL.Advanced.Definition.CSharpALFormatting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConvertingTests
+{
+    [TestClass]
+    public class CodeUnit_ALFToCSharp
+    {
+        [TestMethod]
+        public void CodeUnit_ALFToCSharp_Empty()
+        {
+            var text = File.ReadAllText(@"..\..\..\examples\CSharp-ALFormatting\CodeUnit.cs");
+
+            var tree = CSharpALTree.Parse(text);
+            var target = tree.Convert<CSharpTree>();
+
+            var result = target.ToText();
+
+            //Assert.IsTrue(txt.EndsWith("}"));
+            //Assert.IsTrue(txt.Length > 250);
+
+            //var evaluateA = File.ReadAllText(@"..\..\evaluation\CodeUnit_Empty.cs").Split(
+            //    new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            //var evaluateB = txt.Split(
+            //    new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Assert.AreEqual(evaluateA.Length, evaluateB.Length);
+
+            //for (int i = 0; i < evaluateA.Length; i++)
+            //    Assert.AreEqual(evaluateA[i].Trim(), evaluateB[i].Trim());
+        }
+    }
+}
